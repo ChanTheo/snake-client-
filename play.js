5fa828d2b27da1d30183cd11f05b79f28b74e577
ChanTheo <theo.james.chan@gmail.com>
2020-01-15 19:51:38 +0000

    Added a setupInput function that listens for a user event on the keyboard and also a function that allows the user to exit from the game if they press control + c

--- play.js
+++ play.js
@@ -3,6 +3,24 @@ const { connect } = require('./client');
 console.log('Connecting......')
 connect();
 
+const setupInput = function (){
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  
+  return stdin;
+}
+
+const handleUserInput = function (input){
+  if (input === '\u0003') {
+    process.exit();
+  }
+}
+const stdin = setupInput();
+
+stdin.on('data', handleUserInput);
 /**
  * Establishes connection with the game server
  */
